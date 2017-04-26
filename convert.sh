ee1b1a257227471a47d55e16d40825b748be4f09
Alexandr Gussalov <xolseg@gmail.com>
2017-04-26 11:03:04 +0300

    Add config file

--- convert.sh
+++ convert.sh
@@ -2,29 +2,23 @@
 red='\033[0;31m'
 green='\033[0;32m' 
 normal='\033[0m'
-echo -n "Enter user > "
-read user
-echo -n "Enter node > "
-read node
+source ./config.cfg
+
+	echo -n "Enter node > "
+	read node
+	
 if (exec 3<>/dev/tcp/$node/22) 2> /dev/null; then
 	echo -en "${green}[OK]${normal} \n"
-	echo -n "Enter storage > " 
-	read storage
-	echo -n "Enter export directory > "
-	read folder
-elif (ssh $user@$storage 'test -d '/$folder''); then
-	echo -e "${green}[OK]${normal} "
-	echo -n "Enter interface virtual machine > "
-	read iface
 	echo -n "Enter virtual machine name >"
 	read vmName
 
-echo -e "virt-v2v -ic qemu+ssh://${green}$user${normal}@${green}$node${normal}/system -o rhev -os ${green}$storage${normal}:/${green}$folder${normal} --network ${green}$iface${normal} ${green}$vmName${normal}"
+	echo -e "virt-v2v -ic qemu+ssh://${green}$user${normal}@${green}$node${normal}/system -o rhev -os ${green}$storage${normal}:/${green}$folder${normal} --network ${green}$iface${normal} ${green}$vmName${normal}"
 tput sgr0
-echo -n "Continue? (y/n) "
+	echo -n "Continue? (y/n) "
 else 
 	echo -e "${red}[FAIL]${normal}"
 fi
+
 read item
 case "$item" in
 		y|Y);;
